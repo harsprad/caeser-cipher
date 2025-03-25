32c476f74ced7ded6691ce0a6c6332d99502f622
harsprad <101750118+harsprad@users.noreply.github.com>
2025-03-25 15:11:33 +0000

    Spec: tested main and translate method

--- lib/caeser.rb
+++ lib/caeser.rb
@@ -1,15 +1,17 @@
 def caeser_cipher(message, shift_factor)
   cipher_key = create_cipher(shift_factor)
-  message.chars.map do |letter|
-    if letter.match?(/[^a-zA-Z]/)
-      letter
-    elsif letter.upcase == letter
-      cipher_key[letter.downcase].upcase
-    else
-      cipher_key[letter]
-    end
-  end
+  message.chars.map { |letter| single_translate(letter, cipher_key) }
     .join
+end
+
+def single_translate(letter, cipher_key) 
+  if letter.match?(/[^a-zA-Z]/)
+    letter
+  elsif letter.upcase == letter
+    cipher_key[letter.downcase.to_sym].upcase
+  else
+    cipher_key[letter.to_sym]
+  end
 end
 
 def create_cipher(shift_factor)
@@ -19,7 +21,7 @@ def create_cipher(shift_factor)
                        alphabet
                      else
                        alphabet[shift_factor].upto('z').to_a
-                                             .concat('a'.upto(alphabet[shift_factor - 1]).to_a)
+                         .concat('a'.upto(alphabet[shift_factor - 1]).to_a)
                      end
   alphabet.map.with_index { |letter, i| [letter, shifted_alphabet[i]] }.to_h
 end
--- spec/caeser_spec.rb
+++ spec/caeser_spec.rb
@@ -1,1 +1,53 @@
-relative_require '../lib/caeser.rb'
+require_relative '../lib/caeser.rb'
+
+describe 'Caeser cipher methods' do
+  describe '#caeser_cipher' do
+    let(:message){ "Hello, World!" }
+    let(:shift){ 4 }
+
+    before do
+      allow(self).to receive(:create_cipher)
+      allow(self).to receive(:single_translate).and_return('a')
+    end
+
+    it 'calls create_cipher method' do
+      expect(self).to receive(:create_cipher).with(shift).once
+      caeser_cipher(message, shift)
+    end
+
+    it 'calls single_translate method' do
+      expect(self).to receive(:single_translate).exactly(message.length).times
+      caeser_cipher(message, shift)
+    end
+  end
+
+  describe '#single_translate' do
+    let(:key){ {'a': 'x', 'b': 'y', 'c': 'z'} }
+
+    it 'translates lowercase letter' do
+      letter = 'a'
+      translated_letter = single_translate(letter, key)
+      expect(translated_letter).to eq('x')
+    end
+
+    it 'translates uppercase letter' do
+      letter = 'B'
+      translated_letter = single_translate(letter, key)
+      expect(translated_letter).to eq('Y')
+    end
+
+    it 'translates symbols' do
+      symbol = '!'
+      translated_symbol = single_translate(symbol, key)
+      expect(translated_symbol).to eq('!')
+    end
+  end
+
+  describe '#create_cipher' do
+    let(:alphabet){ 'a'.upto('z').to_a }
+    let(:alpha_h){ 'h'.upto('z').to_a.concat('a'.upto('g').to_a) }
+    let(:alpha_y){ ['y', 'z'].concat('a'.upto('x').to_a) }
+
+
+  end
+end
